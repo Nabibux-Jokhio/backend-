39bacf5c06890ec0e155b0ef9de79321fdcdabbc
Nabibux-Jokhio <jokhionb12@gmail.com>
2025-01-17 15:53:24 +0500

    create user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,19 @@
 import { Router } from "express"
 import { registerUser } from "../controllers/user.controller.js"
+import { upload } from "../middlewares/multer.middlewares.js"
 
 const userRoutes = Router()
 
-userRoutes.post("/register", registerUser)
+userRoutes.post("/register", upload.fields([
+    {
+        name: "avater",
+        maxCount: 1
+    },
+    {
+        name: "coverImage",
+        maxCount: 1
+    },
+]), registerUser)
 // userRoutes.post("/login", login)
 
 export default userRoutes
